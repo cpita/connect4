6a36f06263b45dbbff4168c4cb69cb7293dadb03
Carlos Pita Amérigo <carlospitaamerigo@MacBook-Pro-de-Carlos.local>
2020-08-21 11:35:09 +0200

    Updates

---
+++ precache-manifest.56fc50196c3ba944530156eaf6e75c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e53cc30ef54965ce2bff19a7b66a06",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "5829265c4ca13be808ae",
+    "url": "/connect4/static/css/main.0044347a.chunk.css"
+  },
+  {
+    "revision": "72656b9fe7f52e18feed",
+    "url": "/connect4/static/js/2.768e5b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/connect4/static/js/2.768e5b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5829265c4ca13be808ae",
+    "url": "/connect4/static/js/main.50f9864a.chunk.js"
+  },
+  {
+    "revision": "441aad0d974576b797a9",
+    "url": "/connect4/static/js/runtime-main.4a1f6665.js"
+  }
+]);
